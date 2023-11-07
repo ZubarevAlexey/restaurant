de6c749fa2f40691a44990a66972717f2ef36ed2
User <stparchkg@gmail.com>
2023-11-07 20:40:59 +0600

    add dtos for dish

---
+++ restaurant/src/main/java/kg/java/restaurant/core/models/dtos/dish/DishDto.java
@@ -0,0 +1,20 @@
+package kg.java.restaurant.core.models.dtos.dish;
+
+import kg.java.restaurant.core.models.dtos.category.CategoryDto;
+import kg.java.restaurant.core.models.dtos.category.CategorySearchDataDto;
+import kg.java.restaurant.core.models.dtos.user.UserDto;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class DishDto {
+    private String name;
+    private Double coast;
+    private CategoryDto category;
+
+
+
+}
